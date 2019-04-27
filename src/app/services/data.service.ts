dc99f9430be373ecb034f9ff2a3d3f87a8f578c9
Alexander Zhidkov <azhidkov@gmail.com>
2019-04-27 18:33:29 +0800

    add data service

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  getFaculties(): string[] {
+    return [
+      'Business, Operations/Consulting',
+      'Marketing',
+      'Accounting',
+      'Human Resources'
+    ];
+  }
+}
