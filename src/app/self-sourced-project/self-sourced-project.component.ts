8edc4b14e8e99ca83ee462f9e7c88e4db4b03c59
Alexander Zhidkov <azhidkov@gmail.com>
2019-05-04 17:36:35 +0800

    #8 generate docx with docxtemplater

--- src/app/self-sourced-project/self-sourced-project.component.ts
+++ src/app/self-sourced-project/self-sourced-project.component.ts
@@ -3,6 +3,10 @@ import { UserProfile } from '../model/user-profile';
 import { ActivatedRoute } from '@angular/router';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import * as JSZipUtils from 'jszip-utils';
+import * as JSZip from 'jszip';
+import * as docxtemplater from 'docxtemplater';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-self-sourced-project',
@@ -49,5 +53,47 @@ export class SelfSourcedProjectComponent implements OnInit {
 
   bindFormControls() {
     // ToDo: implement similar to others
+  }
+
+  loadFile(url, callback) {
+    JSZipUtils.getBinaryContent(url, callback);
+  }
+  // https://docxtemplater.readthedocs.io/en/latest/installation.html#browser
+  // https://docxtemplater.readthedocs.io/en/latest/generate.html
+  // https://docxtemplater.com/demo/#simple
+  public generateDocument() {
+    this.eoi = this.eoiDoc.valueChanges();
+    this.eoi.subscribe(data => {
+
+      this.loadFile('./assets/Student Placement Arrangement.docx', (error, content) => {
+        if (error) { throw new Error(error); }
+
+        const zip = new JSZip(content);
+        const doc = new docxtemplater().loadZip(zip);
+        doc.setData({
+          student_name: data.student.name,
+        });
+        try {
+          // render the document (replace all occurrences of {first_name} by John, {last_name} by Doe, ...)
+          doc.render();
+        } catch (error) {
+          const e = {
+            message: error.message,
+            name: error.name,
+            stack: error.stack,
+            properties: error.properties,
+          };
+          console.log(JSON.stringify({ error: e }));
+          // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
+          throw error;
+        }
+        const out = doc.getZip().generate({
+          type: 'blob',
+          mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        });
+
+        saveAs(out, `Student Placement Arrangement - ${data.student.name}.docx`);
+      });
+    });
   }
 }
